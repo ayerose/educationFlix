fcbc0867e212664f19dd23f7fc4f1a915a9aa03c
ayerose <reya-k@outlook.de>
2022-04-09 18:56:03 +0100

    Initial Commit

---
+++ eduFlix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eduFlix
+//
+//  Created by Soreya Koura on 09.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eduFlix/eduFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  eduFlixApp.swift
+//  eduFlix
+//
+//  Created by Soreya Koura on 09.04.22.
+//
+
+import SwiftUI
+
+@main
+struct eduFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
